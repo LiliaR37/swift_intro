7de4abeb35efd07aad2038eb326f76f05afad10b
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-28 10:13:28 -0300

    clases variables

--- variables.playground/Contents.swift
+++ variables.playground/Contents.swift
@@ -6,4 +6,21 @@ var currentLoginAttemp = 0
 
 var x = 0.0, y = 0.0, z = 0.0
 
+//type annotations
+var WelcomeMessage : String
+WelcomeMessage = "Hola"
+
+var red, green, blue : Double
+
+//Posibilidades de nomenclatura de variables(no usadas solo ejemplo)
+let π = 3.141516
+
+var 👽 = "Alien"
+👽 = "Alien2"
+print(👽)
+
+
+
+
+
 
