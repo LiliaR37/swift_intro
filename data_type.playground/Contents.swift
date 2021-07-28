3abdf397179485fad3f9f88d0f49fb94be5cb4ab
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-07-28 13:37:10 -0300

    booleans

---
+++ data_type.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//Booleans
+let orangesAreOrange = true
+let foodIsDelicious = false
+
+var isAged : Bool
+
+isAged = false
+
+if isAged{
+    print("Puedes entrar en la fiesta")
+}else{
+    print("No puedes pasar a la fiesta ")
+}
+
+var age = 31
+if age >= 18{
+    print("Puedes entrar en la fiesta")
+}else {
+    print("No puedes pasar a la fiesta ")
+}
