5f8ea20009a8e5454d234d6a2381814344f5ca57
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-02 12:13:00 -0300

    operations

---
+++ operations.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+let b = 10
+var a = 5
+
+
+let (x,y) = (1,2)
+
+if a == b {
+    print("Los valores son igual ")
+} else {
+    print("No son iguales" )
+}
+
+1+2
+5-3
+2*3
+10.0/2.5
+
+"Hello " + "World"
+
+//D/d -> D == d*c+r
+9/4 //c
+9%4 //r
+
+//Comparacion
+9 == 4*2+1
+
+let five = 5
+let minusFive = -five
+
+let minusSix = -6
+let alsoMinusSix = +minusSix
+
+var number = 5
+number += 3 //number = number + 3
+number -= 2 //number = number - 2
