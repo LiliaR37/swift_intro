4b8f81973f9b81a8cc8b8f8c0c470d6f8bc4c3ca
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-02 10:56:06 -0300

    errors

--- errors.playground/Contents.swift
+++ errors.playground/Contents.swift
@@ -1,3 +1,69 @@
 import UIKit
 
+func canThrowError() throws{
+    //aquí hay codigo que puede causar un error
+}
 
+
+do{
+    try canThrowError()
+    //si llegamos aquí, no ha habido error
+}catch{
+    //si llegamos aquí, ha habido un error...
+}
+
+/*
+ func makeASandwich() throws{
+     
+ }
+ do{
+     try makeASandwich()
+     //Me como el sandwich
+ }catch{
+     //tengo platos limpios... -> Lavar los platos
+     //tengo ingredientes -> Ir a hace la compra
+     //no tengo hambre -> esperar dentro de una hora
+ }
+
+ 
+ */
+
+// Reto
+var cleanDishes = 0
+var ingredientes = 10
+var haveHungry: Bool = true
+
+enum sandwichError: Error{
+    case noCleanDishes
+    case noIngredients
+    case noHungry
+}
+
+func makeASandwich() throws{
+    if cleanDishes < 1 {
+        throw sandwichError.noCleanDishes
+    }
+    if ingredientes < 4 {
+        throw sandwichError.noIngredients
+    }
+    if haveHungry == false {
+        throw sandwichError.noHungry
+    }
+    print("Me hago un sandwich")
+}
+
+
+do{
+    try makeASandwich()
+    //Me como el sandwich
+    print("Me como el sandwich")
+}catch(sandwichError.noCleanDishes){
+    //tengo platos limpios ...-> Lavar los platos
+        print("lava 1 plato")
+}catch (sandwichError.noIngredients){
+    //tengo ingredientes -> Ir a hacer la compra
+        print("compra ingredientes")
+}catch (sandwichError.noHungry){
+    //tengo hambre -> Esperar a tener hambre
+        print("Esperar a tener hambre")
+}
