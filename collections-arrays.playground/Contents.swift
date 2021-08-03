cc4b82f3866247ef7b1905c4495b61743a976f13
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-03 15:11:43 -0300

    arrays

--- collections-arrays.playground/Contents.swift
+++ collections-arrays.playground/Contents.swift
@@ -27,3 +27,40 @@ shoppingList.count
 //Reto
 var family : [String] = ["Maria","Victor","Daniela","Pepe"]
 family.count
+
+//Verificar si está vacío
+if shoppingList.isEmpty {
+    print("La lista de la compra está vacía")
+} else{
+    print("Mandemos a Ricardo a comprar")
+}
+
+//Agregando un elemento al final
+shoppingList.append("Gaseosa")
+shoppingList.count
+
+//Otra forma de agregar al final del array
+shoppingList += ["guacamole","tacos","tomates"]
+shoppingList.count
+
+//Consultar la posicion de los elementos
+var firstElement = shoppingList[0]
+
+//Asignar un elemento a la posicion
+shoppingList[0] = "Huevos"
+shoppingList
+
+//Consultar por rangos
+shoppingList[4...6]
+
+//Asignar por rangos
+shoppingList[2...4] =  ["Naranjas","Mango","Uva"]
+shoppingList
+
+//Eliminar elementos
+let pepper = shoppingList.remove(at: 1)
+shoppingList
+
+//Para eliminar el primero o el ultimo
+let _ = shoppingList.removeLast() //removeFirst()
+shoppingList
