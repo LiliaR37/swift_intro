5b898713abfff2ac050ff86237627b995277ff1b
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-07 10:24:03 -0300

    for, while y switch

--- control_Flow.playground/Contents.swift
+++ control_Flow.playground/Contents.swift
@@ -52,6 +52,70 @@ for time in stride(from: startTime, to: hours, by: hourInterval) {
     }
 }
 
+//While
+var i = 0
+while i <= 10 {
+    i += 1
+    print(i)
+}
 
+var countPrime = 0
+for num in 2...100{
+    for idx in 1...num{
+        if num % idx == 0{
+            countPrime += 1
+        }
+    }
+    if countPrime <= 2{
+        print(num)
+    }else{
+        countPrime = 0
+    }
+}
 
+//If Else
+var temp = 30
+if temp <= 15{ //temp <= 15
+    print("Hace frío! Enciende la calefacción")
+}else if temp >= 25{//temp > 25
+    print("Hace calor! Encendamos el aire acondicionado")
+}else{// 15 < temp < 25
+     print("La sensación térmica es agradable. No hace falta modificarla")
+}
 
+//reto
+var money = 60
+
+if money <= 10  {
+    print("Si, me gusta. Dar Dinero")
+}else if money <= 50  {
+    print("Dar más dinero")
+}else if money <= 100 {
+    print("Eh no, mucho dinero")
+} else {
+    print("Lo siento, no tengo dinero")
+}
+
+//Switch case
+
+let someCharacter: Character = "Z"
+switch someCharacter {
+    case "a", "A":
+        print("Es la primera letra del alfabeto")
+    case "z", "Z":
+        print("Es la última letra del alfabeto")
+    default:
+        print("Es alguna otra letra")
+}
+//reto
+let letter = "e"
+
+switch letter {
+case "a","A","e","E","i","I","o","O","u","U":
+    print("La letra ingresada es una vocal")
+case "b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m","M","n","N", "ñ","Ñ","p", "P","q", "Q","r", "R","s", "S", "t","T", "v","V","w","W", "x","X", "y","Y","Z":
+    print("La letra ingresada es una consonante")
+    
+default:
+    print("El valor ingresado no es permitido. Se requiere una letra")
+}
