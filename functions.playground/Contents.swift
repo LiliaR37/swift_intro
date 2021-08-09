f126b8659114e3fec85e2561aafbce26e267d1da
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-09 11:49:36 -0300

    funciones

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+func greeting(person: String) -> String {
+    let greet = "¡Bienvenue, \(person)!"
+    return greet
+}
+
+greeting(person: "Ricardo Celis")
+greeting(person: "Edgar")
+greeting(person: "Juan Gabriel")
+
