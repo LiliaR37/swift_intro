77cfc4c3c4decd46ec65e459a07da630bc48cc15
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-09 11:17:00 -0300

    agregando switch

--- control_Flow.playground/Contents.swift
+++ control_Flow.playground/Contents.swift
@@ -59,6 +59,13 @@ while i <= 10 {
     print(i)
 }
 
+//repeat while
+repeat {
+    i += 1
+} while i <= 10
+
+print(i)
+
 var countPrime = 0
 for num in 2...100{
     for idx in 1...num{
@@ -118,4 +125,163 @@ case "b","B","c","C","d","D","f","F","g","G","h","H","j","J","k","K","l","L","m"
     
 default:
     print("El valor ingresado no es permitido. Se requiere una letra")
+}
+
+//Switch usando rangos - Interval matching
+let moons = 62
+let phrase = "lunas en Saturno"
+let naturalCount : String
+switch moons {
+case 0:
+    naturalCount = "No hay"
+case 1, 2, 3, 4:
+    naturalCount = "Hay unas pocas"
+case 5..<12:
+    naturalCount = "Hay bastantes"
+case 12..<100:
+    naturalCount = "Hay decenas de"
+case 100..<1000:
+    naturalCount = "Hay centenares de"
+default:
+    naturalCount = "Hay muchiiiiisimas"
+}
+print("\(naturalCount) \(phrase)")
+
+//Reto
+
+let amount = 400
+let amountPhrase : String
+
+switch amount {
+case 10...50:
+    amountPhrase = "Si, puedo prestarte, no hay problema"
+case 100..<500:
+    amountPhrase = "Bueno, pero me lo regresas eh!"
+case 500..<1_000:
+    amountPhrase = "Eh, no. Es mucho dinero. No tengo tanto."
+default:
+    amountPhrase = "Lo siento, no tengo dinero"
+    
+}
+print(amountPhrase)
+
+//Switch usando tuplas
+let somePoint = (5,-8)
+switch somePoint {
+case (0,0):
+    print("El punto \(somePoint) es el origen de coordenadas")
+case (_, 0):
+    print("El punto \(somePoint) se halla sobre el eje de las X")
+case (0, _):
+    print("El punto \(somePoint) se halla sobre el eje de las Y")
+case (-2...2, -2...2):
+    print("El punto \(somePoint) se halla en el interior del cuadrado de lado 4")
+default:
+    print("El punto \(somePoint) está en algún otro lado")
+}
+
+
+
+let anotherPoint = (2,2)
+switch anotherPoint {
+case (let x, 0):
+    print("Sobre el eje de las X, con valor \(x)")
+case (0, let y):
+    print("Sobre el eje de las Y, con valor \(y)")
+case let (x,y) where x == y:
+    print("El punto se halla sobre la recta x = y")
+case let (x,y) where x == -y:
+    print("El punto se halla sobre la recta x = -y")
+case let (x,y):
+    print("En algún otro lugar del plano, en (\(x), \(y))")
+}
+
+
+let stillAnotherPoint = (0,5)
+switch stillAnotherPoint {
+case (let distance, 0), (0, let distance):
+    print("Se halla sobre el eje, a distancia \(distance) del origen")
+default:
+    break
+}
+
+
+let deliveryState: (String, String, Int) = (Estado: "Entregado", Horario: "A tiempo", Distancia: 49 )
+let status: String
+
+// Reto
+switch deliveryState {
+case ("Entregado", "A tiempo", 0..<50):
+    status = "El pedido a sido entregado a tiempo y cerca al punto GPS registrado"
+case ("Entregado", "Retrasado", 0..<50):
+    status = "El pedido a sido entregado con retraso y cerca al punto GPS registrado"
+case ("Pendiente",_, _):
+    status = "El pedido todavia no a sido entregado"
+case ("Entregado", "A tiempo", 50...100):
+    status = "El pedido a sido entregado a tiempo, lejano al punto GPS registrado"
+case ("Entregado", "Retrasado", 50...100):
+    status = "El pedido a sido entregado con retraso, lejano al punto GPS registrado"
+default:
+    status = "Por favor ponerse en contacto con nuestros operadores"
+}
+
+print(status)
+
+
+//Control Transfer Sentences - continue, break, fallthrough, return, throw
+
+//continue, break
+let sentence = "las mentes grandes piensan igual"
+var filteredSentence = ""
+let charactersToRemove:[Character] = ["a", "e", "i", "o", "u"]
+for ch in sentence{
+    if charactersToRemove.contains(ch){
+        continue
+    }
+    filteredSentence.append(ch)
+    if ch == "d"{
+        break
+    }
+}
+filteredSentence
+
+
+
+let integerToDescribe = 5
+var description = "El número \(integerToDescribe) es"
+switch integerToDescribe {
+case 2,3,5,7,11,13,17,19:
+    description += " un número primo, y"
+    fallthrough
+default:
+    description += " un número entero."
+}
+print(description)
+
+
+//guard - return
+var people = ["name": "Juan Gabriel", "age": 31, "isMale": true] as [String : Any]
+
+func testUserValidation(person:[String: Any]){
+    guard let surname = person["surname"] else{
+        print("El apellido es desconocido")
+       return
+    }
+    //aquí existe surname
+    print(surname)
+    guard let age = person["age"] else{
+        print("La edad es desconocida")
+        return
+    }
+    print("La edad de la persona es \(age)")
+}
+
+people["surname"] = "Gomila"
+testUserValidation(person: people)
+
+//Chequeo de compatibilidad
+if #available(iOS 12, macOS 10.12, *){
+    //Ejecutar las acciones a lo iOS12+, a los macOS12+
+}else{
+    //Mantener el código viejo con versiones anteriores de iOS, macOS...
 }
