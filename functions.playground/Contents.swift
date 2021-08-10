9251a816020296dcb514d49c01407510cab3c4d8
LiliaR37 <lalocadeloslentes14@gmail.com>
2021-08-10 11:50:45 -0300

    diferentes tipos de funciones

--- functions.playground/Contents.swift
+++ functions.playground/Contents.swift
@@ -90,3 +90,127 @@ func printWithoutCounting(string: String){
 
 printAndCount(string: "Hola que tal")
 printWithoutCounting(string: "Hola que tal")
+
+//Por medio de una tupla
+func minMax(array:[Int]) -> (min: Int, max: Int){
+    
+  
+    
+    var currentMin = array[0]
+    var currentMax = array[0]
+    
+    for value in array[1..<array.count]{
+        if value < currentMin{
+            currentMin = value
+        }else if value > currentMax{
+            currentMax = value
+        }
+    }
+    
+    return (currentMin, currentMax)
+}
+
+let bounds = minMax(array: [6,3,-8,3,1,9,5,15,-9])
+print("Los valores se hallan entre \(bounds.min) y \(bounds.max)")
+
+
+//Etiquetas de Params y Variadics
+
+func someFunction(f1 firstParamName: Int, f2 secondParamName:Int = 6){
+    //firstParamName variable de tipo Int
+       //secondParamName variable de tipo Int
+       print(firstParamName + secondParamName)
+}
+
+someFunction(f1: 5, f2: 2)
+someFunction(f1: 4)
+
+//Con la _ se omite en la funcion y de todas formas es facil de leer
+func greeting(_ person: String,from hometown: String) -> String {
+    return "Hola \(person) un placer que no visites desde \(hometown)"
+}
+greeting("Juan Gabriel", from: "Mallorca")
+
+
+//Variable variadica
+func mean(_ numbers: Double...) -> Double {
+    var total : Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+
+mean(1,2,3,4,5)
+
+//Reto
+
+func meanGeometric(_ numbers: Double...) -> Double {
+    var total: Double = 1
+    
+    for number in numbers {
+        total *= number
+    }
+    return pow(total, (1/Double(numbers.count)))
+}
+
+meanGeometric(3,3,3)
+
+//Parametros de entrada y salida
+func swapTwoInts(_ a: inout Int,_ b: inout Int){
+    let tempA = a
+    a = b
+    b = tempA
+}
+
+var someInt = 3
+var otherInt = 7
+print("someInt vale \(someInt) y otherInt vale \(otherInt)")
+swapTwoInts(&someInt, &otherInt)
+print("someInt vale \(someInt) y otherInt vale \(otherInt)")
+
+//uso de funciones con otras
+func addTwoInts(_ a: Int, _ b: Int) -> Int {
+    return a+b
+} //(Int, Int) -> Int
+
+func multiplyTwoInts(_ a: Int, _ b: Int ) -> Int{
+    return a*b
+}//(Int, Int) -> Int
+
+func printHW(){
+    print("Hello World")
+} //() -> Void
+
+var mathFunction: (Int, Int) -> Int = multiplyTwoInts
+mathFunction(4,5)
+
+
+
+func printMathResult(_ mathFunc: (Int, Int) -> Int, _ a: Int, _ b: Int){
+    print("Resultado: \(mathFunction(a,b))")
+}
+
+printMathResult(multiplyTwoInts, 5, 9)
+
+//Funciones dentro de otras funciones
+func chooseStepFunction(backward: Bool ) -> (Int) -> Int {
+    
+    func stepForward(_ input: Int) -> Int {
+        return input + 1
+    }
+
+    func stepBackward(_ input: Int) -> Int {
+        return input - 1
+    }
+       
+    return backward ? stepBackward : stepForward
+}
+
+var value = 7
+let moveNearerZero = chooseStepFunction(backward: value > 0)
+while value != 0 {
+    print("\(value)...")
+    value = moveNearerZero(value)
+}
+print("Cero!!!!!!")
